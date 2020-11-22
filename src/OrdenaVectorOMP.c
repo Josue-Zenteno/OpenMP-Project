f2305d68f10f6d8c768c74e2c97b3f6cf7bc6e9d
spicy <josuotp@gmail.com>
2020-11-22 19:02:12 +0100

    Task 2 completed

--- src/OrdenaVectorOMP.c
+++ src/OrdenaVectorOMP.c
@@ -22,14 +22,23 @@ float vini[VECT_SIZE], vord0[VECT_SIZE], vord[VECT_SIZE];	// vector desordenado 
 
 void copiarVector(float Vdest[], float V[], int size)
 {
-  int i;
-  for (i = 0; i < size; ++i)	Vdest[i] = V[i];
+	int i,np;
+	np = omp_get_num_procs();
+	#pragma omp parallel for num_threads(np)
+		for (i = 0; i < size; ++i){	
+			Vdest[i] = V[i];
+		}
 }
 
 void printVector(float vector[], int size)
 {
-  for (int i = 0; i < size; ++i)	printf("%0.4f\t", vector[i]);
-  printf("\n\n");
+	/**********************************************************************************************
+	*NON PARALLELIZABLE FOR LOOP 
+	As we cannot set an order of execution for the threads, the previous and the ordered vector 
+	are printed randomly depending on the 'speed' of each thread printing each value.
+	***********************************************************************************************/
+	for (int i = 0; i < size; ++i) printf("%0.4f\t", vector[i]);
+	printf("\n\n");
 }
 
 int estaOrdenado(float vector[], int size)
@@ -43,13 +52,16 @@ int estaOrdenado(float vector[], int size)
 int vectoresIguales(float vecta[], float vectb[], int size)
 {
 	int i, ini, fin, iguales = TRUE, np = omp_get_num_procs();	// número de núcleos del microprocesador
-	for (i=0; i<np; i++)
-	{
-		ini = i*size/np;	fin =(i+1)*size/np;
-		int j=ini;
-		while ((vecta[j] == vectb[j]) && (j<fin)) j++;
-		iguales = iguales && (j==fin);
-	}
+	#pragma omp parallel for num_threads(np) private(ini, fin, i) shared(iguales)
+		for (i=0; i<np; i++)
+		{
+			ini = i*size/np;	fin =(i+1)*size/np;
+			int j=ini;
+			while ((vecta[j] == vectb[j]) && (j<fin)) j++;
+			
+			#pragma omp critical
+			iguales = iguales && (j==fin);
+		}
 	return iguales;
 }
 
@@ -70,6 +82,17 @@ void mezcla_ordenada(float vector[], int ini1, int ini2, int fin2)
 					/* Rotamos vector[i], vector[i+1], ...,vector[j-1], vector[j]
 					 * para que vector[j] pase a la posición i y el resto se desplace una posición a la derecha */
 					temp = vector[j];
+					/**********************************************************************************************
+					*NON PARALLELIZABLE FOR LOOP
+					The following for loop cannot be parallelized, as, for example in the iteration k=3
+					we will be reading vector[3-1] and writing this value in vector[3] then in the next iteration
+					we will reading vector[2-1] and writing in vector[2] that is the same memory position where we
+					were reading in the previous iteration, so there exist a Write after Read dependecy (WAR) and 
+					as we cannot set an order of execution for the threads, this loop cannot be parallelized.
+
+					[There are WAR dependences in the whole loop unless it is the first iteration where there will 
+					be only one thread]
+					***********************************************************************************************/
 					for (k=j; k>i ; k--) vector[k] = vector[k-1];
 					vector[i] = temp;
 					if (j==fin2) terminado = 1;
@@ -88,17 +111,24 @@ void mezcla_ordenada(float vector[], int ini1, int ini2, int fin2)
 
 void ord_parA(float vector[], int size)
 {
-	int incr, i, fin2;
+	int incr, i, fin2,np;
+	np = omp_get_num_procs();
+	/**********************************************************************************************
+	*NON PARALLELIZABLE FOR LOOP
+	As all the threads will be executing at the same time one thread can be ordering the vector by
+	pair of 2 values, other can be ordering the vector by sets of 4 and so on so on.
 
+	[Parallelizing this has no sense as we will be repeating the work in every thread.]
+	***********************************************************************************************/
 	for (incr = 2; incr < 2*size; incr = 2*incr){
-		#pragma omp parallel for private (i,fin2) shared(vector,size,incr)
-		for (i = 0; i < (size-incr/2); i += incr)		// (i+incr/2) < size
-		{	fin2 = min(size-1,i+incr-1);
-			mezcla_ordenada(vector,i,i+incr/2,fin2);	
-			/* Quedan ordenados incr componentes consecutivos del vector (índices i,i+1,...i+incr-1)
-			 * (el último trozo ordenado puede ser menor: índices i, i+1,...size-1)
-			*/
-		}
+		#pragma omp parallel for num_threads(np) private (fin2, i) shared(vector,size,incr)
+			for (i = 0; i < (size-incr/2); i += incr)		// (i+incr/2) < size
+			{	fin2 = min(size-1,i+incr-1);
+				mezcla_ordenada(vector,i,i+incr/2,fin2);	
+				/* Quedan ordenados incr componentes consecutivos del vector (índices i,i+1,...i+incr-1)
+				* (el último trozo ordenado puede ser menor: índices i, i+1,...size-1)
+				*/
+			}
 		/* El último valor de incr cumple size <= incr < 2*size. 
 		 * Esto conlleva que está ordenado todo el vector, al ser size <= incr */
 	}
@@ -108,26 +138,40 @@ void ord_parA(float vector[], int size)
 void ord_secB(float vector[], int size)
 {
   int i,j; float x;
+  /**********************************************************************************************
+	*NON PARALLELIZABLE FOR LOOP
+	The following for loop cannot be parallelized, as, for example in the iteration i = 2, j will
+	be j = 1, so in the while loop we read vector[1] checking the condition, then inside this loop
+	we assign to vector[2] the value of vector[1]. Then in the next iteration i = 3, j will be
+	j = 2 and then in the while loop we will be reading vector[2] which was written in the previous
+	iteration. For theis reason we have a Read after Write dependency (RAW).
 
-  #pragma parallel private(j) shared(i,x)
-  for (int i = 1; i < size; i++){
-	x = vector[i];
-	#pragma atomic  
-	j = i-1;
-	while ((x<vector[j])&&(0<=j))
-	{
-		#pragma critical
-		vector[j+1]=vector[j]; j--;
-	}
-	vector[j+1]=x;
-	//Critical hasta aqui
+	[There are RAW dependences in the whole loop unless it is the first iteration.]
+	***********************************************************************************************/  
+  for (int i = 1; i < size; i++)
+  {
+	 x = vector[i]; j = i-1;
+	 while ((x<vector[j])&&(0<=j))
+	 {
+		 vector[j+1]=vector[j]; j--;
+	 }
+	 vector[j+1]=x;
   }
 }	// Fin de ord_secB
 
 void ord_secC(float vector[], int size)
 {
    int list_length, i; float temp;
-   
+   /**********************************************************************************************
+	*NON PARALLELIZABLE FOR LOOP
+	As we cannot set an order of execution for the threads we cannot assure that the algorithm will
+	work properly a 100% of the times, as in some cases (with the example of the assignment) if one
+	thread is executing the algorithm with a lenght of 6 and then it comes another one executing 
+	the algorithm with a lenght of 7. Therefore 32 will be set in the second to last position and it 
+	will not be accesible for any other thread. This leads the algorithm to fail.
+
+	[Take into account that this cannot be fixed by protecting the shared variables]
+	***********************************************************************************************/
    for (list_length = size; list_length >= 2; list_length--)
    /* El siguiente for no se puede paralelizar ya que, por ejemplo, en la iteración i=0 
     * puede que haya que escribir en vector[1] y en la iteración siguiente (i=1) hay que leer vector[1].
@@ -177,16 +221,18 @@ void ord_parD(float vector[], int size)
 
 int main()
 {
-	int i; double t;
+	int i, np; double t;
 	
 	// 1. Dar valores aleatorios al vector en el intervalo [0, M[
 	srand((unsigned)time(NULL));
 	float faux;
-	for (i=0; i < VECT_SIZE; i++)	{
-		faux = (float)(rand()%RAND_MAX)/RAND_MAX;
-		// faux se distribuye con igual probabilidad a lo largo del intervalo [0,1[
-		vini[i] = M *faux;
-	}
+	np = omp_get_num_procs();
+	#pragma omp parallel for num_threads(np) shared(vini) private(i,faux)
+		for (i=0; i < VECT_SIZE; i++){
+			faux = (float)(rand()%RAND_MAX)/RAND_MAX;
+			// faux se distribuye con igual probabilidad a lo largo del intervalo [0,1[
+			vini[i] = M *faux;
+		}
 		
 	// 2. Imprimir vector desordenado (solo si el número de componentes no es muy grande)
 	if (VECT_SIZE <= 400)	{
@@ -198,7 +244,20 @@ int main()
 	//	  Para el resto de métodos, copiar vini en vord, ordendar vord y comprobar que vord queda igual que vord0
 	//	  Para todos los métodos medir e imprimir el tiempo de copiar y ordenar el vector
 
-	
+	/**********************************************************************************************
+	*NON PARALLELIZABLE FOR LOOP
+	As the program has been made in a way that we have to wait for the algorithm A to be executed 
+	to compare its result with the other Algorithm results and then print if those have been ordered
+	correctly too. 
+
+	For that reason as we will have all the threads executing at the same time if the thread responsible
+	of executing the algorithm A is not the very first ending its execution and saving it in vord0, 
+	then all the other threads will finish before and also checking their results with an old 
+	version of vord0, printing that they are not correct. But they, in fact, are ordered.
+
+	[Take into account that we cannot use barriers as the threads will execute a different part of code
+	due to the switch.]
+	***********************************************************************************************/
 	for (i=0; i<NM; i++)
 	{
 		t = omp_get_wtime();
@@ -237,8 +296,8 @@ int main()
 				ord_parD(vord,VECT_SIZE);
 				printf("\nTiempo empleado por método paralelo D: %0.8f milisegundos\n",1000*(omp_get_wtime()-t));
 				if (vectoresIguales(vord0, vord, VECT_SIZE))
-					printf("\nEl vector obtenido por el método paralelo D coincide con el del método secuencial A\n");
-				else printf("\nEl vector obtenido por el método paralelo D no coincide con el del método secuencial A\n");		
+					printf("\nEl vector obtenido por el método paralelo D coincide con el del método paralelo A\n");
+				else printf("\nEl vector obtenido por el método paralelo D no coincide con el del método paralelo A\n");			
 				break;				
 		}
 	}
